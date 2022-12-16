69edb7a0e4a515495567d19e5c96113defa7dfac
t-riku <dingariku@icloud.com>
2022-12-16 11:00:45 +0900

    ✨ headerコンポーネントを追加

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "src/components/Header/Header.module.scss";
+
+const Header = () => {
+  return <div className={classes.header}>Header</div>;
+};
+
+export default Header;
