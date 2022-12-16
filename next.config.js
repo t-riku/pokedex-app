19e8425839384c5a725713bbe9e385d32ee73462
t-riku <dingariku@icloud.com>
2022-12-16 16:46:01 +0900

    💡 コメントを追加

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  // images: {
+  //   // remotePatterns: [
+  //   //   {
+  //   //     // protocol: "https",
+  //   //     // hostname: "images/raw.githubusercontent.com",
+  //   //     // port: "",
+  //   //     // pathname: "/account123/**",
+  //   //     domains: ["assets.example.com"],
+  //   //   },
+  //   // ],
+  //   domains: ["assets.example.com"],
+  // },
+  images: {
+    domains: ["assets.example.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
