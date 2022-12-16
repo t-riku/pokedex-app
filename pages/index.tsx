2be316d34da93b0ec87721bf96b344e52fed1ff7
t-riku <dingariku@icloud.com>
2022-12-16 10:34:17 +0900

    🎉 Next.js + TypeScript + Sass + eslint + の環境構築

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "../styles/Home.module.scss";
+import style from "./index.module.scss";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -20,13 +21,14 @@ export default function Home() {
             Get started by editing&nbsp;
             <code className={styles.code}>pages/index.tsx</code>
           </p>
+          <p className={style.scss_test}>Scss Test</p>
           <div>
             <a
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '}
+              By{" "}
               <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
@@ -119,5 +121,5 @@ export default function Home() {
         </div>
       </main>
     </>
-  )
+  );
 }
